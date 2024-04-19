ffe9bc43e29d4ddeefd3ff6102766470bbabb023
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2024-04-19 18:36:58 -0400

    added zod & Product info

---
+++ src/app/admin/_actions/products.ts
@@ -0,0 +1,18 @@
+"use server"
+
+import { z } from "zod"
+
+const fileSchema = z.instanceof(File, { message: "Required" })
+const imageSchema = fileSchema.refine(file )
+
+z.object({
+    name: z.string().min(1),
+    description: z.string().min(1),
+    priceInCents: z.coerce.number().int().min(1),
+    file: fileSchema.refine(file => file.size > 0, "Required"),
+    image: fileSchema.refine(file => file.size > 0, "Required"),
+})
+
+export async function addProduct(formData: FormData) {
+    console.log(formData)
+}
