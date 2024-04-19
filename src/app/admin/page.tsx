97fb33ea0e717f0e4a8ba50a2a61c3007272d1f0
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2024-04-19 12:42:36 -0400

    fix schema, and formatter

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,34 +1,87 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import db from '@/db/db';
+import { formatCurrency, formatNumber } from '@/lib/formatters';
 
-function getSalesDate(){
-     
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { priceInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
 }
 
-export default function AdminDashboard () {
-    return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-       <DashboardCard title="Sales" subtitle="Test" body="body" />
-       <DashboardCard title="Sales" subtitle="Test" body="body" />
-       <DashboardCard title="Sales" subtitle="Test" body="body" />
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: { priceInCents: true },
+    }),
+  ]);
+  return {
+    userCount,
+    averageValuePerUser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+  };
+}
 
+export default async function AdminDashboard() {
+  const [salesData, userData] = await Promise.all([
+    getSalesData(),
+
+    getUserData(),
+  ]);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard
+        title="Sales"
+        subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
+        body={formatCurrency(salesData.amount)}
+      />
+      <DashboardCard
+        title="Customers"
+        subtitle={`${formatCurrency(
+          userData.averageValuePerUser
+        )} Average Value`}
+        body={formatNumber(userData.userCount)}
+      />
+      <DashboardCard
+        title="Active Products"
+        subtitle={`${formatNumber()} Inacctive`}
+        body={formatNumber()}
+      />
     </div>
+  );
 }
 
 type DashboardCardProps = {
-    title: string
-    subtitle: string
-    body: string
-}
+  title: string;
+  subtitle: string;
+  body: string;
+};
 
-function DashboardCard({title, subtitle, body}:
-    DashboardCardProps
-) {
-    return  <Card>
-    <CardHeader>
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
         <CardTitle>{title}</CardTitle>
-    <CardDescription>{subtitle}</CardDescription>
-        </CardHeader>
-    <CardContent>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
         <p>{body}</p>
-    </CardContent>
-</Card>
+      </CardContent>
+    </Card>
+  );
 }
