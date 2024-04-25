bcf04a4219f6f205fb7a77b59dc8905e8360ccbe
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2024-04-25 09:52:46 -0400

    building Table Cells

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,7 +1,17 @@
 import { Button } from '@/components/ui/button';
 import { PageHeader } from '../_components/PageHeader';
 import Link from 'next/link';
-import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import db from '@/db/db';
+import { CheckCircle2, MoreVertical, XCircle } from 'lucide-react';
+import { formatCurrency, formatNumber } from '@/lib/formatters';
 
 export default function AdminProductsPage() {
   return (
@@ -17,24 +27,60 @@ export default function AdminProductsPage() {
   );
 }
 
-function ProductsTable() {
+async function ProductsTable() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      priceInCents: true,
+      isAvailableForPurchase: true,
+      _count: { select: { orders: true } },
+    },
+    orderBy: { name: 'asc' },
+  });
+
+  if (products.length === 0) return <p>No products found</p>;
+
   return (
     <Table>
       <TableHeader>
         <TableRow>
           <TableHead className="w-0">
-            <span className="sr-only">Available for Purchase</span>
+            <span className="sr-only">Available For Purchase</span>
           </TableHead>
           <TableHead>Name</TableHead>
           <TableHead>Price</TableHead>
           <TableHead>Orders</TableHead>
           <TableHead className="w-0">
             <span className="sr-only">Actions</span>
-          </TableHead>{' '}
+          </TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        
+        {products.map((product) => (
+          <TableRow key={product.id}>
+            <TableCell>
+              {product.isAvailableForPurchase ? (
+                <>
+                  <span className="sr-only">Available</span>
+                  <CheckCircle2 />
+                </>
+              ) : (
+                <>
+                  <span className="sr-only">Unavailable</span>
+                  <XCircle className="stroke-destructive" />
+                </>
+              )}
+            </TableCell>
+            <TableCell>{product.name}</TableCell>
+            <TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+            <TableCell>{formatNumber(product._count.orders)}</TableCell>
+            <TableCell>
+              <MoreVertical />
+              <span className='sr-only'>Action</span>
+            </TableCell>
+          </TableRow>
+        ))}
       </TableBody>
     </Table>
   );
